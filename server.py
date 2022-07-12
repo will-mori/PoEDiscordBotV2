d9dc781c3a477e58df82251fac543e4d699cf994
William Mori <39865148+will-mori@users.noreply.github.com>
2022-07-11 18:51:21 -0700

    Initial File Commit

---
+++ server.py
@@ -0,0 +1,261 @@
+# William Mori
+
+import os
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+import aiohttp
+import asyncio
+import random
+from datetime import date, datetime, timezone
+
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
+CURRENCY = ["Chaos Orb", "Exalted Orb", "Divine Orb", "Awakener's Orb","Elevated Sextant","Ancient Orb","Orb of Annulment", "Stacked Deck", "Awakened Sextant"]
+CURRENCY_DICT = {"c": "Chaos Orb", "ex": "Exalted Orb", "woke": "Awakener's Orb", "esex": "Elevated Sextant",
+                 "anc": "Ancient Orb", "annul":"Orb of Annulment", "deck": "Stacked Deck", "asex": "Awakened Sextant",
+                 "od": "Orb of Dominance", "oc": "Orb of Conflict", "ec": "Eldritch Chaos Orb",
+                 "eex": "Eldritch Exalted Orb", "eoa": "Eldritch Orb of Annulment", "do": "Divine Orb",
+                 "unmake": "Orb of Unmaking", "fuse": "Orb of Fusing", "tcat": "Tempering Catalyst",
+                 "fcat": "Fertile Catalyst", "gcp": "Gemcutter's Prism", "alt": "Orb of Alteration",
+                 "alch": "Orb of Alchemy", "vaal": "Vaal Orb"
+                 }
+
+CURRENCY_STR = ", ".join(CURRENCY_DICT.keys())
+
+
+
+bot = commands.Bot(command_prefix='!')
+
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} has connected to Discorcd')
+
+'''
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    if message.content == "woah":
+        response = "Woah"
+        await message.channel.send(response)
+    elif message.content == "raise-exception":
+        raise discord.DiscordException
+'''
+
+
+@bot.command(name="woah", help="woah bro")
+async def woah(ctx, mg=None):
+    if ctx.author == bot.user:
+        return
+    print(ctx)
+    print(mg)
+    response = "Woah"
+    await ctx.channel.send(response)
+
+
+@bot.command(name="pokemon", help="Returns a random gen 1 pokemon")
+async def pokemon(ctx):
+    number = random.randint(1, 151)
+    url = f"https://pokeapi.co/api/v2/pokemon/{number}"
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as resp:
+            pokemon = await resp.json()
+            await ctx.channel.send(pokemon['name'])
+
+
+@bot.command(name="trade")
+async def trade(ctx):
+    query = {
+        "sell": "Exalted Orb",
+        "buy": "Chaos Orb",
+        "limit": 5
+    }
+    url = "http://trade.maximumstock.net/trade"
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url, json=query) as resp:
+            print(resp)
+            output = await resp.json(content_type="application/json")
+            for i in output["offers"]:
+                del i["item_id"]
+                del i["stash_id"]
+                del i["seller_account"]
+                print(i)
+
+            await ctx.channel.send("look at terminal")
+
+
+def limit_handler(arg: str) -> (bool, int):
+    """
+    Helper function to handle converting argument
+    :param arg: String to be cast to an int and used as the query limit
+    :return: Tuple with first element representing if the arg is valid, the second representing the valid argument
+                if the first element is false, ignore the second element
+    """
+    try:
+        limit = int(arg)
+        if limit < 1:
+            return False, limit
+        if limit > 200:
+            limit = 200
+        return True, limit
+    except ValueError:
+        return False, 0
+
+
+def currency_handler(arg: str) -> (bool, str):
+    """
+    Helper function to convert currency as passed by the user to a currency as used by the endpoint
+    :param arg: String to be looked up in CURRENCY_DICT
+    :return: Tuple with the first element representing if the arg is a valid currency, the secong element representing
+                the currency to obe used in the query, if the first element is false, ignore the second element
+    """
+    try:
+        return True, CURRENCY_DICT[arg]
+    except KeyError:
+        return False, arg
+
+
+@bot.command(name="query", help="Query a currency exchange rate.  Command can be used in the following forms:\n1."
+                                "'!query {currency}' to see generic listings of the currency using the default "
+                                "listing limit\n"
+                                "2. '!query {currency} {currency}' to see listings of the first currency using"
+                                " the second currency using the default listing limit\n"
+                                "3. '!query {currency} {limit}' to see generic listings with the number of listings "
+                                "limited by limit\n"
+                                "4. '!query {currency} {currency} {limit}' to see listings of the first currency"
+                                "to be purchased with the second currency with the number of listings limited by limit")
+async def query(ctx, *args):
+    if len(args) == 0:
+        await ctx.channel.send("Must specify a currency to query from " + str(list(CURRENCY_DICT.keys())))
+        return
+
+    valid, sell = currency_handler(args[0])
+    if valid is False:
+        await ctx.channel.send(f"Invalid first argument {args[0]}, enter a valid currency for the first argument."
+                               f"  Use !currency to see the full currency list")
+        return
+
+    query = {"sell": sell}
+
+    if len(args) == 2:
+        valid_buy, buy = currency_handler(args[1])
+        if valid_buy is False:
+            valid_limit, limit = limit_handler(args[1])
+            if valid_limit is False:
+                await ctx.channel.send(f"Invalid second argument {args[1]}, enter a currency, or enter an integer from"
+                                       f" 1 to 200, integers over 200 will be truncated to 200.  Use !currency to see"
+                                       f" the full currency list.")
+                return
+            else:
+                query["limit"] = limit
+        else:
+            query["buy"] = buy
+
+    elif len(args) == 3:
+        valid_buy, buy = currency_handler(args[1])
+        if valid_buy is False:
+            await ctx.channel.send(f"Invalid second argument {args[1]}, enter !currency to see the full currency list")
+            return
+        valid_limit, limit = limit_handler(args[2])
+        if valid_limit is False:
+            await ctx.channel.send(f"Invalid limit {args[2]}, enter an integer from 1 to 200, integers over 200 will"
+                                   f"be truncated to 200")
+        query["buy"] = buy
+        query["limit"] = limit
+
+    elif len(args) > 3:
+        await ctx.channel.send(f"Too many arguments, args has len {len(args)}")
+        return
+
+
+    print(query)
+
+    url = "http://trade.maximumstock.net/trade"
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url, json=query) as resp:
+            print(resp)
+            output = await resp.json(content_type="application/json")
+            for i in output["offers"]:
+                del i["item_id"]
+                del i["stash_id"]
+                del i["seller_account"]
+                print(i)
+
+            await ctx.channel.send("look at terminal")
+
+
+@bot.command(name="currency", help="Prints out a list of searchable currencies")
+async def currency_print(ctx):
+    await ctx.channel.send(CURRENCY_STR)
+
+
+@bot.command(name="log-currency", help="Logs prices of ex, mirrors, woke, and ancient orbs")
+async def log_currency(ctx):
+    start = datetime.now()
+    queries = [
+        {
+            "sell": "Exalted Orb",
+            "buy": "Chaos Orb",
+            "limit": 200
+        },
+        {
+            "sell": "Mirror of Kalandra",
+            "buy": "Exalted Orb",
+            "limit": 200
+        },
+        {
+            "sell": "Awakener's Orb",
+            "buy": "Exalted Orb",
+            "limit": 200
+        },
+        {
+            "sell": "Ancient Orb",
+            "buy": "Exalted Orb",
+            "limit": 200
+        }
+    ]
+    url = "http://trade.maximumstock.net/trade"
+    log = []
+    async with aiohttp.ClientSession() as session:
+        for i in queries:
+            async with session.post(url, json=i) as resp:
+                output = await resp.json(content_type="application/json")
+                for listing in output["offers"]:
+                    del listing["item_id"]
+                    del listing["stash_id"]
+                    del listing["seller_account"]
+                    log.append(listing)
+
+    file_name = str(date.today())
+    file_time = str(datetime.now().isoformat())
+    file = open(file_name, "w")
+    file.write(file_time)
+    for i in log:
+        file.write(str(i)+",\n")
+    file.close()
+    await ctx.channel.send("Finished in "+str(datetime.now()-start))
+
+
+@bot.command(name="file")
+async def ftest(ctx):
+    file_name = str(date.today())
+    time_now = str(datetime.now().replace(microsecond=0).isoformat())
+    file = open(file_name, "x")
+    file.write("hello")
+
+
+@bot.event
+async def on_error(event, *args, **kwargs):
+    with open("err.log",'a') as file:
+        if event == "on_message":
+            file.write(f"Unhandled message: {args[0]}")
+        else:
+            raise
+
+
+bot.run(TOKEN)
+
+
